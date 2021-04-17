5468f0d027519a3297d40eb3300a6be4b2e5e136
Glover <glover@Glovers-MacBook-Pro.local>
2021-04-17 21:25:19 +0100

    feat: theme switcher

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,34 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import styles from "./Layout.module.css";
 import Link from "next/link";
+import { Brightness6Rounded } from "@material-ui/icons";
 
 const Layout = ({ children, title = "World Online" }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      localStorage.getItem("theme")
+    );
+
+    setTheme(localStorage.getItem("theme"));
+  }, []);
+
+  const switchTheme = () => {
+    if (theme === "light") {
+      saveTheme("dark");
+    } else {
+      saveTheme("light");
+    }
+  };
+
+  const saveTheme = (theme) => {
+    setTheme(theme);
+    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -13,12 +39,16 @@ const Layout = ({ children, title = "World Online" }) => {
         <Link href='/'>
           <span className={styles.logo}>LOGO.</span>
         </Link>
+
+        <button className={styles.themeSwitcher} onClick={switchTheme}>
+          <Brightness6Rounded />
+        </button>
       </header>
 
       <main className={styles.main}>{children}</main>
 
       <footer className={styles.footer}>
-        Glover @ {new Date().getFullYear()}
+        Glover, {new Date().getFullYear()}
       </footer>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,12 +21,16 @@ export default function Home({ countries }) {
 
   return (
     <Layout>
-      <div className={styles.counts}>Found {countries.length} countries</div>
+      <div className={styles.inputContainer}>
+        <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <SearchInput
-        placeholder='Filter by Name, Region or SubRegion'
-        onChange={handleInputChange}
-      />
+        <div className={styles.input}>
+          <SearchInput
+            placeholder='Filter by Name, Region or SubRegion'
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
 
       <CountriesTable countries={filteredCountries} />
     </Layout>
